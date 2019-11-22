dadf65faa9ed267b4bf0c20f09a4691f265e79f5
Durgesh2398 <2308durgesh@gmail.com>
2019-11-22 12:07:51 +0530

    added 1

--- newsjs.js
+++ newsjs.js
@@ -90,3 +90,49 @@ function news1(){
         }
     }
 }    
+function news2(){
+    var country = document.getElementById("country").value;
+    var url = "https://newsapi.org/v2/top-headlines?country="+country+"&category=business&apiKey=bdceeaeb320c4e2bafb98c5bad6375ad";
+    var xtpp = new XMLHttpRequest();
+    xtpp.open("GET", url, true);
+    xtpp.send();
+    xtpp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            var resul = this.responseText;
+            //console.log(result);
+            var jsonresul = JSON.parse(resul);
+            console.log(jsonresul);
+            //let list = document.getElementById('list');
+            let output= "";
+            let latestNew =jsonresul.articles;
+            for (var i in latestNew) {
+                output += `<div class="col l4 m6 s12">
+                            <div class="card medium hoverable">
+                                <div class="card-image">
+                                    <img src="${latestNew[i].urlToImage}" class="responsive-img" style="width:300px;height:300px;" alt="${latestNew[i].title}">
+                                </div>
+                                <div class="card-content">
+                                    <span class="card-title activator"><i class="material-icons right">more_vert</i></span>
+                                    <h6 class="truncate">Title: <a href="${latestNew[i].url}" title="${latestNew[i].title}">${latestNew[i].title}</a></h6>
+                                    <p><b>Author</b>: ${latestNew[i].author} </p>
+                                    <p><b>News source</b>: ${latestNew[i].source.name} </p>
+                                    <p><b>Published</b>: ${latestNew[i].publishedAt} </p>
+                                </div>
+
+                                <div class="card-reveal">
+                                    <span class="card-title"><i class="material-icons right">close</i></span>
+                                    <p><b>Description</b>: ${latestNew[i].description}</p>
+                                </div>
+
+                                <div class="card-action">
+                                    <a href="${latestNew[i].url}" target="_blank" class="btn">Read More</a>
+                                </div>
+                            </div>
+                        </div>`;
+            }
+            if (output !== "") {
+                $("#newsResults").html(output);
+            }
+        }
+    }
+}
